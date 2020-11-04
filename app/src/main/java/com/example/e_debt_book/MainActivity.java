3b14f1ebc5462d53baca3f1c456d31d461cd6ec4
kalikali <bilalo.ggg@gmail.com>
2020-11-05 01:12:46 +0300

    Creating Login UI version 1

--- app/src/main/java/com/example/e_debt_book/MainActivity.java
+++ app/src/main/java/com/example/e_debt_book/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.e_debt_book;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,22 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    ////Main screen Choice attributes
+    private ConstraintLayout mainChoice;
+    private Button mainMarketButton;
+    private Button mainCostumertButton;
+
+
+    ////Main screen costumer login attribute
+    private ConstraintLayout costumerLogin;
+
+
+
+    ////Main screen Market login attribute
+    private ConstraintLayout marketLogin;
+
+
+
     private TextView textView;
     private Button who;
     private String text;
@@ -29,13 +46,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ////Main screen Choice attributes
+        mainChoice = findViewById(R.id.mainChoice);
+        mainMarketButton = findViewById(R.id.mainMarketButton);
+        mainCostumertButton = findViewById(R.id.mainCostumertButton);
+
+        ////Main screen costumer login attribute
+        costumerLogin = findViewById(R.id.costumerLogin);
+
+
+        ////Main screen Market login attribute
+        costumerLogin = findViewById(R.id.costumerLogin);
+        marketLogin = findViewById(R.id.marketLogin);
+
+        mainCostumertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainChoice.setVisibility(View.GONE);
+                costumerLogin.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        mainMarketButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainChoice.setVisibility(View.GONE);
+                marketLogin.setVisibility(View.VISIBLE);
+            }
+        });
+/*
         firebaseDatabase = FirebaseDatabase.getInstance();
         mRootRef = FirebaseDatabase.getInstance().getReference();
         conditionRef = mRootRef.child("Costumers").child("456165").child("first_name");
         who = (Button)findViewById(R.id.who);
-        textView = (TextView)findViewById(R.id.textView);
+        textView = (TextView)findViewById(R.id.textView);*/
     }
-
+/*
     @Override
     protected void onStart() {
         super.onStart();
@@ -51,13 +98,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
         who.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textView.setText(text);
             }
         });
-    }
+    }*/
 }
