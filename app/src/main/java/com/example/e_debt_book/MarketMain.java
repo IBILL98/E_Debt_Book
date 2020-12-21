e4779d3045534d254ffb85e57a293ef5e8795def
Alhamza <e170503110@stud.tau.edu.tr>
2020-12-21 19:35:40 +0300

    added edit market interface

--- app/src/main/java/com/example/e_debt_book/MarketMain.java
+++ app/src/main/java/com/example/e_debt_book/MarketMain.java
@@ -1,14 +1,68 @@
 package com.example.e_debt_book;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MarketMain extends AppCompatActivity {
 
+    Button mainMarketAddCustomertButton ,mainMarketeditinfoButton , mainMarketmyCostumertButton ,mainMarketLogoutButton;
+
+    ConstraintLayout passwordconfirmationmeny , mainmarketmenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_main);
+
+        mainMarketAddCustomertButton = findViewById(R.id.mainMarketAddCustomertButton);
+        mainMarketeditinfoButton = findViewById(R.id.mainMarketeditinfoButton);
+        mainMarketmyCostumertButton = findViewById(R.id.mainMarketmyCostumertButton);
+        mainMarketLogoutButton = findViewById(R.id.mainMarketLogoutButton);
+
+
+
+
+
+        mainMarketAddCustomertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mainMarketeditinfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mainMarketmyCostumertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mainMarketLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/e_debt_book/editMarketInfo.java
@@ -0,0 +1,18 @@
+package com.example.e_debt_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editMarketInfo extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_market_info);
+
+
+
+    }
+}
