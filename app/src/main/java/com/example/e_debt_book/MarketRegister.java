8fd28b34d574af99df08a68be7efccd5ae7cb5d7
kalikali <bilalo.ggg@gmail.com>
2020-12-22 11:00:20 +0300

    Edited some stuff in the main screen

--- app/src/main/java/com/example/e_debt_book/MarketRegister.java
+++ app/src/main/java/com/example/e_debt_book/MarketRegister.java
@@ -3,12 +3,30 @@ package com.example.e_debt_book;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MarketRegister extends AppCompatActivity {
+
+    EditText costumerRegisterName,costumerRegisterLastName,costumerRegisterPassword,costumerRegisterPhone,costumerRegisterEmail;
+    Button costumerRegisterBackButton,costumerRegisterSignUpButtom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_register);
+
+        costumerRegisterName = findViewById(R.id.costumerRegisterName);
+        costumerRegisterLastName = findViewById(R.id.costumerRegisterLastName);
+        costumerRegisterPassword = findViewById(R.id.costumerRegisterPassword);
+        costumerRegisterPhone = findViewById(R.id.costumerRegisterPhone);
+        costumerRegisterEmail = findViewById(R.id.costumerRegisterEmail);
+        costumerRegisterBackButton = findViewById(R.id.costumerRegisterBackButton);
+        costumerRegisterSignUpButtom = findViewById(R.id.costumerRegisterSignUpButtom);
+
+
+
+
     }
 }
