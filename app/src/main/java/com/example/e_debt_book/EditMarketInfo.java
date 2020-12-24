50f5c2def6fbef225b72117f3614bccf7376bc63
kalikali <bilalo.ggg@gmail.com>
2020-12-24 12:38:04 +0300

    edit market information edited ;P

--- app/src/main/java/com/example/e_debt_book/EditMarketInfo.java
+++ app/src/main/java/com/example/e_debt_book/EditMarketInfo.java
@@ -1,30 +1,16 @@
 package com.example.e_debt_book;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.e_debt_book.model.Market;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-import java.util.Map;
-//Firebase attributes
+import com.google.firebase.database.DatabaseReference;
 
 
 public class EditMarketInfo extends AppCompatActivity {
@@ -32,9 +18,6 @@ public class EditMarketInfo extends AppCompatActivity {
     EditText editInfoName , editInfoEmail  , editInfoPassword, editInfoIban , editInfoAdress;
     Button marketeditBackButton2, marketeditDoneButton2;
 
-
-    FirebaseAuth fAuth;
-    FirebaseDatabase firebaseDatabase;
     DatabaseReference mRootRef,conditionRef;
 
 
@@ -83,14 +66,6 @@ public class EditMarketInfo extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
-
-
-
 
 
 
