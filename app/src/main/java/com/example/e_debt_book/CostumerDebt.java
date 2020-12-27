540155741870152657d7ca10f9a80b76c43047c6
aslannvolkan <e160501126@stud.tau.edu.tr>
2020-12-27 17:41:31 +0300

    Portakal

---
+++ app/src/main/java/com/example/e_debt_book/CostumerDebt.java
@@ -0,0 +1,14 @@
+package com.example.e_debt_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CostumerDebt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_costumer_debt);
+    }
+}
