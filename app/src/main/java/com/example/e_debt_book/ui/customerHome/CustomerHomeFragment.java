ae3cd79287ea6ebf64ed8d13bb66495ae3227d3d
kalikali <bilalo.ggg@gmail.com>
2020-12-27 15:08:21 +0300

    first Try Bilal ;)

--- app/src/main/java/com/example/e_debt_book/ui/customerHome/CustomerHomeFragment.java
+++ app/src/main/java/com/example/e_debt_book/ui/customerHome/CustomerHomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.e_debt_book.ui.customerHome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +17,20 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.e_debt_book.R;
+import com.example.e_debt_book.addNewDebt;
+import com.example.e_debt_book.debtsDetails;
+import com.example.e_debt_book.model.Customer;
+import com.example.e_debt_book.model.Debt;
+import com.example.e_debt_book.model.Market;
+import com.example.e_debt_book.myCustomers;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class CustomerHomeFragment extends Fragment {
 
@@ -24,6 +42,9 @@ public class CustomerHomeFragment extends Fragment {
                 new ViewModelProvider(this).get(CustomerHomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_customer_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+
+
+
         customerHomeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
