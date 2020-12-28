f1f0111176a7d253323e47a2113ba2c6f97e0846
Alhamza <e170503110@stud.tau.edu.tr>
2020-12-28 14:34:59 +0300

    edit market phone number XML

---
+++ app/src/main/java/com/example/e_debt_book/EditMarketPassword.java
@@ -0,0 +1,14 @@
+package com.example.e_debt_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditMarketPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_market_password);
+    }
+}
