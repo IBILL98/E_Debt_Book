2c6d5c9946aac594f7c8b2786fefa6703602c604
kalikali <bilalo.ggg@gmail.com>
2020-12-28 14:00:03 +0300

    Costumer edited To CUSTOMER

--- app/src/main/java/com/example/e_debt_book/ui/customerProfile/CustomerProfileFragment.java
+++ app/src/main/java/com/example/e_debt_book/ui/customerProfile/CustomerProfileFragment.java
@@ -1,35 +1,52 @@
 package com.example.e_debt_book.ui.customerProfile;
 
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.e_debt_book.R;
+import com.example.e_debt_book.model.Customer;
+
+import com.google.firebase.database.DatabaseReference;
+
 
 public class CustomerProfileFragment extends Fragment {
 
-    private CustomerProfileViewModel customerProfileViewModel;
+    EditText editCostomerName, editCostomerLastName, editCostomerPassword, editCostomerPhone, editCostomerEmail;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        customerProfileViewModel =
-                new ViewModelProvider(this).get(CustomerProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_customer_editprofile, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        customerProfileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container , Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_customer_editprofile, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Customer customer = (Customer) getActivity().getIntent().getSerializableExtra("Customer");
+
+
+        editCostomerEmail = getView().findViewById(R.id.editCostomerEmail);
+        editCostomerPhone = getView().findViewById(R.id.editCostomerPhone);
+        editCostomerPassword = getView().findViewById(R.id.editCostomerPassword);
+        editCostomerLastName = getView().findViewById(R.id.editCostomerLastName);
+        editCostomerName = getView().findViewById(R.id.editCostomerName);
+
+        editCostomerEmail.setText(customer.getEmail());
+        editCostomerPhone.setText(customer.getPhone());
+        editCostomerLastName.setText(customer.getLastname());
+        editCostomerName.setText(customer.getName());
+
+
+
+
+
     }
 }
