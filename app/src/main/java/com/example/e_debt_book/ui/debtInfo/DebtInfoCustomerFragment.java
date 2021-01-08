ffc67d88504f0d6e9eb855edcf33c2429615df0a
kalikali <bilalo.ggg@gmail.com>
2021-01-08 19:10:07 +0300

    created a UI for CustomerDebt Inofrmations

--- app/src/main/java/com/example/e_debt_book/ui/debtInfo/DebtInfoCustomerFragment.java
+++ app/src/main/java/com/example/e_debt_book/ui/debtInfo/DebtInfoCustomerFragment.java
@@ -1,29 +1,55 @@
 package com.example.e_debt_book.ui.debtInfo;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintJob;
+import android.print.PrintManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.Printer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.e_debt_book.R;
 import com.example.e_debt_book.model.Customer;
 import com.example.e_debt_book.model.Debt;
 import com.example.e_debt_book.model.Item;
 import com.example.e_debt_book.model.Market;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class DebtInfoCustomerFragment extends Fragment {
@@ -34,7 +60,6 @@ public class DebtInfoCustomerFragment extends Fragment {
     Button  printButton;
     ImageButton changeDueDateButton;
     ListView listView;
-
     DatabaseReference reference;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,15 +69,15 @@ public class DebtInfoCustomerFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        customerInfoDisplay = getActivity().findViewById(R.id.customerInfoDisplay);
-        amountDisplay = getActivity().findViewById(R.id.amountDisplay);
-        dateOFLoanDisplay = getActivity().findViewById(R.id.dateOFLoanDisplay);
-        dueDateDisplay = getActivity().findViewById(R.id.dueDateDisplay);
-        descriptionDisplay = getActivity().findViewById(R.id.descriptionDisplay);
+        customerInfoDisplay = (TextView) getActivity().findViewById(R.id.customerInfoDisplay);
+        amountDisplay = (EditText) getActivity().findViewById(R.id.amountDisplay);
+        dateOFLoanDisplay = (TextView) getActivity().findViewById(R.id.dateOFLoanDisplay);
+        dueDateDisplay = (EditText) getActivity().findViewById(R.id.dueDateDisplay);
+        descriptionDisplay = (EditText) getActivity().findViewById(R.id.descriptionDisplay);
         changeDueDateButton = getActivity().findViewById(R.id.changeDueDateButton);
-        printButton = getActivity().findViewById(R.id.printButton);
-        listView = getActivity().findViewById(R.id.productsList);
-        LinearLayout content = getActivity().findViewById(R.id.printedLayout);
+        printButton = (Button) getActivity().findViewById(R.id.printButton);
+        listView = (ListView) getActivity().findViewById(R.id.productsList);
+        LinearLayout printedLayout = getActivity().findViewById(R.id.printedLayout);
 
 
         setuneditable();
@@ -65,16 +90,16 @@ public class DebtInfoCustomerFragment extends Fragment {
         reference = FirebaseDatabase.getInstance().getReference().child("Debts");
 
         ArrayList<String> displayarray = new ArrayList<>();
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,displayarray);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, displayarray);
         listView.setAdapter(arrayAdapter);
-        for (int i = 0; i<debt.getItemList().size();i++){
+        for (int i = 0; i < debt.getItemList().size(); i++) {
             Item item = debt.getItemList().get(i);
-            displayarray.add(i,item.toString());
+            displayarray.add(i, item.toString());
             arrayAdapter.notifyDataSetChanged();
 
         }
 
-        customerInfoDisplay.setText(customer.getName()+" "+customer.getLastname()+", "+customer.getPhone());
+        customerInfoDisplay.setText(customer.getName() + " " + customer.getLastname() + ", " + customer.getPhone());
         amountDisplay.setText(debt.getAmount());
         dateOFLoanDisplay.setText(debt.getDateOfLoan());
         dueDateDisplay.setText(debt.getDueDate());
@@ -85,7 +110,6 @@ public class DebtInfoCustomerFragment extends Fragment {
         MaterialDatePicker materialDatePicker = builder.build();
 
 
-
         materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
             @Override
             public void onPositiveButtonClick(Object selection) {
@@ -94,10 +118,44 @@ public class DebtInfoCustomerFragment extends Fragment {
         });
 
 
-
         printButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
+
+                try{
+                    printButton.setVisibility(View.INVISIBLE);
+                    android.graphics.pdf.PdfDocument document = new android.graphics.pdf.PdfDocument();
+
+                    android.graphics.pdf.PdfDocument.PageInfo pageInfo = new android.graphics.pdf.PdfDocument.PageInfo.Builder(printedLayout.getWidth(), printedLayout.getHeight(), 1).create();
+
+                    android.graphics.pdf.PdfDocument.Page page = document.startPage(pageInfo);
+
+                    Canvas canvas = page.getCanvas();
+                    Paint paint = new Paint();
+                    canvas.drawPaint(paint);
+                    printedLayout.draw(canvas);
+                    document.finishPage(page);
+
+                    String path = "/sdcard/"+debt.getDebtID()+".pdf";
+
+                    java.io.File myFile = new java.io.File(path);
+
+                    java.io.FileOutputStream fOut = new java.io.FileOutputStream(myFile);
+
+                    java.io.OutputStreamWriter myOutWriter = new java.io.OutputStreamWriter(fOut);
+
+                    document.writeTo(fOut);
+
+                    document.close();
+                    myOutWriter.close();
+                    fOut.close();
+
+                    Toast.makeText(getActivity(), "File Saved", Toast.LENGTH_LONG).show();
+
+                } catch (Exception e) {
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
 
 
 
@@ -105,11 +163,14 @@ public class DebtInfoCustomerFragment extends Fragment {
 
             }
         });
-
-
-
-
     }
+
+
+
+
+
+
+
 
 
     public void seteditable(){
@@ -143,7 +204,6 @@ public class DebtInfoCustomerFragment extends Fragment {
         changeDueDateButton.setFocusable(false);
         changeDueDateButton.setFocusableInTouchMode(false);
     }
-
 
 
 }
