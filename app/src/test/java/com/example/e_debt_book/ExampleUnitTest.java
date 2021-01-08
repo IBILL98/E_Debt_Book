c44ce67dfb24c56ff9e4459d707199285e264ea8
Alhamza <e170503110@stud.tau.edu.tr>
2021-01-08 10:29:22 +0300

    addMrketTest

--- app/src/test/java/com/example/e_debt_book/ExampleUnitTest.java
+++ app/src/test/java/com/example/e_debt_book/ExampleUnitTest.java
@@ -15,6 +15,62 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 
+///////////////////
+    /*
+
+package com.example.e_debt_book;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import androidx.test.runner.AndroidJUnit4;
+import org.mockito.Mock;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.ViewAction;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+//@Mock Context mockContext
+public class addMarketTest {
+    //Check if Activity is in view
+    @Test
+    public void test1_isMainActivityInView() {
+        ActivityScenario activityScenario = ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.mainChoice)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void test2_clickNewNote(){
+        ActivityScenario activityScenario = ActivityScenario.launch(MainActivity.class);
+
+        //Check if main market button is visible
+        onView(withId(R.id.mainMarketButton)).check(matches(isDisplayed()));
+
+
+    }
+}
+
+
+*/
+    /////////////////
+
+
+
+
+
+
+
 
 
 
